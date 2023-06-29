bc9f46a32dd6739f26d2bd62ff169b7d62dfabcf
4FunkE <4FunkEcodes@gmail.com>
2023-06-29 13:32:49 -0400

    creating shapes

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//if i want to change colores of my console log
+//console.log(`\x1b[36m${text}\x1b[0m`);
+
 // Packages needed for this application
 const fs = require("fs")//to read and write files
 const inquirer = require("inquirer")//the inquiry files from packageJSON
